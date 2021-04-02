899efdb3c5cc71c1433bdcea586cf434d84a3a48
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-01 22:27:58 -0400

    added dictionary component and stylesheet
    will make search engine out of API and display results

---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Dictionary.css";
+import axios from "axios";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <Form.Group>
+        <Form.Control
+          size="lg"
+          type="search"
+          placeholder="Which word do you want to look up?"
+          className="textbox"
+        />
+        <Button variant="info" className="btn">
+          Search
+        </Button>
+      </Form.Group>
+    </div>
+  );
+}
