d36e25f39a31061abec073803550c93c0a014fe0
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-01 22:27:22 -0400

    added a link to repo at bottom of project

---
+++ src/GithubLink.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function GithubLink() {
+    return (
+        <div className="GithubLink">
+        <div title="Link to GitHub Repository" className="text-center" >
+          <a
+            href="https://github.com/P0KiD0Ki/Weather-App-React"
+            target="_blank"
+            rel="noreferrer"
+            className="foot"
+          >
+            <i class="fab fa-github"></i> {""}
+            Coded by Perla Oki
+          </a>
+        </div>
+      </div>
+    )
+}
