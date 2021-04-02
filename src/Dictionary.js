f1cb8029d71dd712248d3ea21819f505028a5e0d
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-02 13:30:54 -0400

    added event listeners to form, removed submission button

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,33 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 import "./Dictionary.css";
 // import axios from "axios";
 
 export default function Dictionary() {
+  const [word, setWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Oh haaaaaayyyyy! Here's what ${word} means:`);
+  }
+
+  function handleSubmit(event) {
+    setWord(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <Form.Group>
-        <Form.Control
-          size="lg"
-          type="search"
-          placeholder="Which word do you want to look up?"
-          className="textbox"
-        />
-        <Button variant="info" className="btn">
-          Search
-        </Button>
-      </Form.Group>
+      <form onSubmit={search}>
+        <Form.Group role="form">
+          <Form.Control
+            size="lg"
+            type="search"
+            placeholder="Which word do you want to look up?"
+            className="textbox"
+            onChange={handleSubmit}
+          />
+        </Form.Group>
+      </form>
     </div>
   );
 }
