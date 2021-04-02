103e49326b24779d921b3a86f082a9af121be0b6
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-02 14:22:22 -0400

    added dictionary API and console logged API response

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import "./Dictionary.css";
-// import axios from "axios";
+import axios from "axios";
 
 export default function Dictionary() {
   const [word, setWord] = useState("");
 
+  function handleResponse(response) {
+      console.log(response.data[0])
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Oh haaaaaayyyyy! Here's what ${word} means:`);
+
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
