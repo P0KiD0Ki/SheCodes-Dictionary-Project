4087313df914a88686a5bd65279d2cf26c1ff997
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-04 21:11:51 -0400

    removed phonetics and added into external component

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
-//   console.log(props.results.phonetics[0].text);
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <h5>{props.results.phonetics[0].text}  <a href="/" alt="listen to the pronunciation"><i class="fas fa-volume-up"></i></a></h5>
+        {props.results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         <br />
         {props.results.meanings.map(function (meaning, index) {
           return (
