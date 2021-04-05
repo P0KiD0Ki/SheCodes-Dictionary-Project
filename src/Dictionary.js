2f4398c312fad28a13f53117e640c8f8a1c16775
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-04 21:09:54 -0400

    added error handling function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,6 +4,7 @@ import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
 
+
 export default function Dictionary() {
   const [word, setWord] = useState("");
   let [results, setResults] = useState(null);
@@ -12,12 +13,16 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
+  function errorFunction() {
+      alert("Oops! Let's try that again.")
+  }
+
   function search(event) {
     event.preventDefault();
 
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponse).catch(errorFunction);
   }
 
   function handleSubmit(event) {
