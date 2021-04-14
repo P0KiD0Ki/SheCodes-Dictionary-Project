da2dd258d79503ca52df9129545d6d7b462eb6d2
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-14 09:54:17 -0400

    Update Phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Results.css"
 
 export default function Phonetics(props) {
     return (
-        <div className="Phonetics">
-            <h5>
+        <div>
+            <h5 className="Phonetics">
           {props.phonetics.text}{" "}
           <a href={props.phonetics.audio} alt="listen to the pronunciation" target="_blank" rel="noreferrer">
             <i class="fas fa-volume-up"></i>
