a1c428c613a235f0c0d0e8da0d64fbf3b648e8c7
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-14 09:53:31 -0400

    added css file and class name

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics";
+import "./Results.css"
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <h2 className="definition">{props.results.word}</h2>
         {props.results.phonetics.map(function (phonetics, index) {
           return (
             <div key={index}>
