a5ad63f12f5fb0aa7e25b8fb9c910c2c8a4d0ccb
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-14 22:57:30 -0400

    updated styling

--- src/Example.js
+++ src/Example.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Results.css"
 
 export default function Examples(props) {
     if (props.example) {
         return(
-            <div>
-                <em><strong>Example:</strong> "{props.example}."</em>
+            <div className="Example">
+                <em>"{props.example}."</em>
             </div>
         )
     } else {
