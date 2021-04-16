fba15630337663c754a0ea4b3913ffd1c5d5c460
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-15 23:31:46 -0400

    failback to previous code

--- src/WodResults.js
+++ src/WodResults.js
@@ -8,28 +8,16 @@ export default function WodResults(props) {
   const [definition, setDefinition] = useState(props.word);
 
   function handleWodResponse1(response) {
-    setPartofspeech(response.data[0].partOfSpeech);
-    setDefinition(response.data[0].text);
+    setTimeout(function () {
+      setPhonetics(response.data[0].phonetics[0].text);
+      setPronunciation(response.data[0].phonetics[0].audio);
+      setPartofspeech(response.data[0].meanings[0].partOfSpeech);
+      setDefinition(response.data[0].meanings[0].definitions[0].definition);
+    }, 2000);
   }
 
-  function handleWodResponse2(response) {
-    setPhonetics(response.data[0].raw);
-  }
-
-  function handleWodResponse3(response) {
-    setPronunciation(response.data[0].fileUrl);
-  }
-
-  let wordnikApiKey = "qn8bh0l6c1agzsxgijmuazcwxzhh8of8u5ehqpgcvenlev28m";
-
-  let apiPronunciationUrl = `https://api.wordnik.com/v4/word.json/${props.word}/audio?useCanonical=false&limit=50&api_key=${wordnikApiKey}`;
-  axios.get(apiPronunciationUrl).then(handleWodResponse3);
-
-  let apiPhoneticUrl = `https://api.wordnik.com/v4/word.json/${props.word}/pronunciations?useCanonical=false&limit=50&api_key=${wordnikApiKey}`;
-  axios.get(apiPhoneticUrl).then(handleWodResponse2);
-
-  let apiDefinitionUrl = `https://api.wordnik.com/v4/word.json/${props.word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=${wordnikApiKey}`;
-  axios.get(apiDefinitionUrl).then(handleWodResponse1);
+  let apiUrl1 = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${props.word}`;
+  axios.get(apiUrl1).then(handleWodResponse1);
 
   return (
     <div>
@@ -49,3 +37,7 @@ export default function WodResults(props) {
     </div>
   );
 }
+
+//definition: response.data[0].meanings[0].definitions[0].definition
+//phonetics: response.data[0].phonetics[0].text
+//partofspeech: response.data[0].meanings[0].partOfSpeech
