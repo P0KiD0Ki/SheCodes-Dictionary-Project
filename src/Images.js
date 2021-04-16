57564cef2ab7a29dfa68d9b5cf58d95234530e55
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-04-15 21:38:15 -0400

    added images component and stylesheet

---
+++ src/Images.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Results.css";
+import "./Images.css";
+
+export default function Images(props) {
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <section>
+      <div className="row">
+        {props.photos.map(function (photo, index) {
+          return (
+            <div className="col-4" key={index}>
+              <span>
+                <a href={photo.largeImageURL} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.largeImageURL}
+                    alt={photo.tags}
+                    className="img-fluid"
+                  ></img>
+                </a>
+              </span>
+            </div>
+          );
+        })}
+      </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
